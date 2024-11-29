072b1db72e0138fed2c0b241b7f6fde7cde71016
Kavindu Udara <155027870+kavindu-udara@users.noreply.github.com>
2024-11-29 18:14:22 +0530

    added lazy loading and loading component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,43 +1,49 @@
 import "./App.css";
+import { lazy, Suspense } from "react";
 import { ToastContainer } from "react-toastify";
-import Register from "./pages/auth/Register";
+const Register = lazy(() => import("./pages/auth/Register"));
 import "react-toastify/dist/ReactToastify.css";
-import Login from "./pages/auth/Login";
+const Login = lazy(() => import("./pages/auth/Login"));
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Profile from "./pages/user/Profile";
-import SingleCoursePage from "./pages/course/SingleCoursePage";
-import TeacherDashboard from "./pages/teacher/TeacherDashboard";
-import CreateCourse from "./pages/teacher/CreateCourse";
-import EditCourse from "./pages/course/EditCourse";
-import EntrollCoursePage from "./pages/course/EntrollCoursePage";
+const Home = lazy(() => import("./pages/Home"));
+const Profile = lazy(() => import("./pages/user/Profile"));
+const SingleCoursePage = lazy(() => import("./pages/course/SingleCoursePage"));
+const TeacherDashboard = lazy(() => import("./pages/teacher/TeacherDashboard"));
+const CreateCourse = lazy(() => import("./pages/teacher/CreateCourse"));
+const EditCourse = lazy(() => import("./pages/course/EditCourse"));
+const EntrollCoursePage = lazy(
+  () => import("./pages/course/EntrollCoursePage")
+);
 import TeacherRoute from "./routes/TeacherRoute";
 import Index from "./pages/LandingPage";
+import LoadingPage from "./pages/LoadingPage";
 
 function App() {
   return (
     <BrowserRouter>
       <ToastContainer />
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
+      <Suspense fallback={<LoadingPage />}>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
 
-        <Route path="/course">
-          <Route path=":id" element={<SingleCoursePage />} />
-          <Route path="entroll/:courseId" element={<EntrollCoursePage />} />
-        </Route>
-
-        <Route element={<TeacherRoute />}>
-          <Route path="/teacher">
-            <Route index element={<TeacherDashboard />} />
-            <Route path="create-course" element={<CreateCourse />} />
-            <Route path="edit-course/:id" element={<EditCourse />} />
+          <Route path="/course">
+            <Route path=":id" element={<SingleCoursePage />} />
+            <Route path="entroll/:courseId" element={<EntrollCoursePage />} />
           </Route>
-        </Route>
-      </Routes>
+
+          <Route element={<TeacherRoute />}>
+            <Route path="/teacher">
+              <Route index element={<TeacherDashboard />} />
+              <Route path="create-course" element={<CreateCourse />} />
+              <Route path="edit-course/:id" element={<EditCourse />} />
+            </Route>
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
---
+++ frontend/src/pages/LoadingPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingPage:React.FC = () => {
+  return (
+    <div className='text-center text-primary-color'>Loading...</div>
+  )
+}
+
+export default LoadingPage
